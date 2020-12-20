ee7a0ab728f2529e6201c4815acfe547d6b61c75
Jarodbf <71373525+Jarodbf@users.noreply.github.com>
2020-12-20 19:26:50 +0100

    Update roulette.js

--- js/roulette.js
+++ js/roulette.js
@@ -24,8 +24,8 @@ function ajouterEl(formulaire)
 }
 
 /**
- * Permet d'afficher les element true du tableu d'objet roul.
- * Une valeur va être
+ * Permet d'afficher les elements true du tableu d'objet roul.
+ * et affiche dans la DOM le résultat
  */
 function affiche()
 {
@@ -47,20 +47,49 @@ function affiche()
 }
 
 /**
+ * Permet d'afficher les elements true du tableu d'objet roul.
+ * utilise un tableau intémédiaire pour l'ordre alphabétique.
+ * et affiche dans la DOM le résultat.
+ */
+function afficheAlph()
+{
+    let temp = "";// valeur de transition pour y prendre tout le text.
+    let alph = [];//Tableau qui va contenire le .sort.
+    if(roulette.length === 0)
+    {
+        document.getElementById("elAffichage").innerText = "\nAucun élément actuellement.";
+    }
+    else {
+        for(let i in roulette)
+        {
+            if(roulette[i].value) {
+                alph.push([roulette[i].name,roulette[i].pos]);// créé le tableau de tableau
+            }
+        }
+        alph.sort();//range par ordre alphaétique
+        for(let i in alph)
+        {
+                temp += ("\nCode: " + alph[i][1] + " Nom: " + alph[i][0]);//position 1 tableau nom position 0 tableau de position.
+        }
+        document.getElementById("elAffichage").innerText = temp;
+    }
+}
+/**
  * Remplacement l'élément selectionner avec l'id par false si l'élément existe et qu'il n'est pas déja false.
+ * suppV est la valeur de position donner par l'utilisateur.
  * @param formulaire
  * @returns {boolean}
  */
 function supp(formulaire)
 {
-    let poss = roulette.length -1;
-    let suppTemp = (formulaire.suppV.value);
-    if((poss< formulaire.suppV.value) || (poss > formulaire.suppV.value) || (roulette[suppTemp] === undefined))
-    {
-        document.getElementById("elAffichage").innerText = "\nAucun élément actuellement.";
-        return false;
-    }
-    else {
+        let poss = roulette.length -1;
+        let suppTemp = (formulaire.suppV.value);
+        if(( formulaire.suppV.value < 0) || (poss < formulaire.suppV.value) || (roulette[suppTemp] === undefined))
+        {
+            document.getElementById("elAffichage").innerText = "\nAucun élément actuellement.";
+            return false;
+        }
+        else {
             if(roulette[suppTemp].value)
             {
                 roulette[suppTemp].value = false;
@@ -73,4 +102,30 @@ function supp(formulaire)
                 return false;
             }
         }
+    //}
+
+}
+
+/**
+ * prend un nombre aléatoir avec un math floor dans les bornes du .lenght
+ * si la valeur a ça value false affiche un message.
+ */
+function roul()
+{
+    if(roulette.length === 0)
+    {
+        document.getElementById("roulAffichage").innerText = "\nAucun élément actuellement.";
+    }
+    else {
+        let aleaRoul = Math.floor(Math.random()*roulette.length);//nombre aléatoire
+        if (roulette[aleaRoul].value)//test si value est === true
+        {
+            document.getElementById("roulAffichage").innerText = ("Code: " + roulette[aleaRoul].pos + "\nNom: " + roulette[aleaRoul].name);
+        }
+        else//message si false
+        {
+            document.getElementById("roulAffichage").innerText = "Vous êtes tomber sur un élément suprimer, retentez votre chance !";
+        }
+
+    }
 }
